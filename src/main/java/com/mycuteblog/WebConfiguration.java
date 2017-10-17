fba4fcb56bdd1a3b69caa558c6f30d9712c85ee1
Angelo Manganiello <angelo.mang@libero.it>
2017-10-17 17:33:30 +0200

    update

--- src/main/java/com/mycuteblog/WebConfiguration.java
+++ src/main/java/com/mycuteblog/WebConfiguration.java
@@ -4,7 +4,7 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-@Configuration
+//@Configuration
 public class WebConfiguration {
     @Bean
     ServletRegistrationBean h2servletRegistration(){
